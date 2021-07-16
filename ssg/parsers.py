581f0e40fa075685ed2b926b98930945d2a1b51f
Jared Schultz <JaredSchultz88@gmail.com>
2021-07-16 16:23:20 -0700

    Fixed ssg/parsers.py missing parenthesis

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -22,11 +22,11 @@ class Parser:
             file.write(content)
 
     def copy(self, path, source, dest):
-        shutil.copy2(path, dest / path.relative_to(source)
+        shutil.copy2(path, dest / path.relative_to(source))
 
 
 class ResourceParser(Parser):
-    extensions=[".jpg", ".png", ".gif", ".css", ".html"]
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
     def parse(self, path, source, dest):
         self.copy(path, source, dest)
