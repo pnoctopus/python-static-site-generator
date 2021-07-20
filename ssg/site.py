d1e8678ea5d82ef938ccdb6797d8d297669f3323
Jared Schultz <JaredSchultz88@gmail.com>
2021-07-20 04:54:11 -0700

    Fixed multiple errors

--- ssg/site.py
+++ ssg/site.py
@@ -33,10 +33,10 @@ class Site:
             parser.parse(path, self.source, self.dest)
         else:
             self.error(
-                "No parser for the '{}' extension, file skipped!".__format__(
+                "No parser for the '{}' extension, file skipped!".format(
                     path.suffix)
             )
 
     @staticmethod
     def error(message):
-        sys.stderr.write("\x1b[1;31m{}\n").__format__(message)
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
